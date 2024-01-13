3329e71b9cef5ef2c9b4bc6bfc4f847702d26acb
Štěpán Granát <granat.stepan@gmail.com>
2024-01-13 19:30:09 +0100

    chore: parser auto build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { exec } from "node:child_process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "build-parser",
+      watchChange(id) {
+        if (id.indexOf("tolgee.grammar") !== -1) {
+          exec("npm run --silent grammar", (_, stdout, stderr) => {
+            if (stdout) {
+              process.stdout.write(stdout);
+            }
+            if (stderr) {
+              process.stderr.write(stderr);
+            }
+          });
+        }
+      },
+    },
+  ],
+});
