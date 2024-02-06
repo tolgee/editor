8169d119c6c04042018d42a0029453e02bdbab9c
Štěpán Granát <granat.stepan@gmail.com>
2024-02-06 15:44:31 +0100

    feat: make placeholders smaller

--- src/parser/placeholdersStyle.ts
+++ src/parser/placeholdersStyle.ts
@@ -47,26 +47,24 @@ export const generatePlaceholdersStyle = ({
   return styled(component)`
     white-space: pre-wrap;
     & .placeholder-widget {
-      display: inline-grid;
+      display: inline-flex;
+      vertical-align: text-top;
       align-items: center;
       justify-content: center;
-      min-height: 20px;
-      max-height: 20px;
+      height: 18px;
       min-width: 15px;
       border: 1px solid ${colors.variable.border};
       background-color: ${colors.variable.background};
       color: ${colors.variable.text};
       border-radius: 10px;
-      padding: 0px 9px;
-      font-size: 14px;
+      padding: 0px 7px;
+      font-size: 12px;
       user-select: none;
       margin: 0px 1px;
       overflow: hidden;
 
       & > * {
         display: grid;
-        height: 21px;
-        margin: -4px 0px -4px 0px;
       }
     }
     & .placeholder-error {
@@ -77,26 +75,26 @@ export const generatePlaceholdersStyle = ({
       border: 1px solid ${colors.tag.border};
       background-color: ${colors.tag.background};
       color: ${colors.tag.text};
-      border-radius: 12px 0px 0px 12px;
-      padding-right: 8px;
+      border-radius: 10px 0px 0px 10px;
+      padding-right: 6px;
     }
 
     & .placeholder-tagClose {
       border: 1px solid ${colors.tag.border};
       background-color: ${colors.tag.background};
       color: ${colors.tag.text};
-      border-radius: 0px 12px 12px 0px;
-      padding-left: 8px;
+      border-radius: 0px 10px 10px 0px;
+      padding-left: 6px;
     }
 
     // in rtl mode, revert the placeholders direction
     &[dir="rtl"] .placeholder-tagOpen {
-      border-radius: 0px 12px 12px 0px;
-      padding-left: 8px;
+      border-radius: 0px 10px 10px 0px;
+      padding-left: 6px;
     }
     &[dir="rtl"] .placeholder-tagClose {
-      border-radius: 12px 0px 0px 12px;
-      padding-right: 8px;
+      border-radius: 10px 0px 0px 10px;
+      padding-right: 6px;
     }
   `;
 };
